5ecc8728a736f04e3b978caadb3678da2b323808
Lemonder791123 <63327445+Lemonder791123@users.noreply.github.com>
2020-04-09 15:14:55 +0800

    Unity_Practice_ok

--- ClassField/Assets/EnemyManger.cs
+++ ClassField/Assets/EnemyManger.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyManger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("怪物一號")]
+    public Enemy enemy1;
+    [Header("怪物二號")]
+    public Enemy enemy2;
+
+    
+    private void Start()
     {
+        print("怪物一號的魔力：" + enemy1._mp);
+        print("怪物二號的裝備名稱：" + enemy2._item);
         
+
+        enemy1._hp      = 100;
+        enemy2._hp      = 300;
+        enemy1._def     = 50;
+        enemy1._weapon  = "木棍";
+        enemy2._weapon  = "短刀";
+        enemy1._key     = true;
+        enemy2._money   = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
